90b713d724bb74cccd2f61c97fcec4e76f161308
krish3957 <kpparekh81602@gmail.com>
2024-04-22 22:53:54 +0530

    Commit

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://task-management-backend-gmyf.vercel.app//api";
 
 
-const user = JSON.parse(localStorage.getItem("persist:root")).user;
-const currentUser = user && JSON.parse(user).currentUser;
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user)?.currentUser;
 const TOKEN = currentUser && currentUser.accesTocken;
 console.log(TOKEN);
 
@@ -15,7 +15,7 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  headers:{token :`Bearer ${TOKEN}`}
+  headers: { token: `Bearer ${TOKEN}` }
 }
 );
 
